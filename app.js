7953f31f1bfdd2604321c93122dff3c85a5b7366
Chris Chen <ok3201123@gmail.com>
2021-08-05 09:50:11 +0800

    feat: print the diff time from request start to response end

--- app.js
+++ app.js
@@ -4,26 +4,37 @@ const app = express()
 const port = 3000
 
 app.use((req, res, next) => {
-  const reqTime = new Date()
-  console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} | ${req.method} from ${req.originalUrl}`)
+  const reqTimestamp = Date.now()
+  res.locals.reqTimestamp = reqTimestamp
   next()
 })
 
-app.get('/', (req, res) => {
+const printLog = (req, res) => {
+  const { reqTimestamp } = res.locals
+  const resTimestamp = Date.now()
+  console.log(`${moment(reqTimestamp).format('YYYY-MM-DD HH:mm:ss')} | ${req.method} from ${req.originalUrl} | total time: ${resTimestamp - reqTimestamp}ms`)
+  res.end()
+}
+
+app.get('/', (req, res, next) => {
   res.send('列出全部 Todo')
-})
+  next()
+}, printLog)
 
-app.get('/new', (req, res) => {
+app.get('/new', (req, res, next) => {
   res.send('新增 Todo 頁面')
-})
+  next()
+}, printLog)
 
-app.get('/:id', (req, res) => {
+app.get('/:id', (req, res, next) => {
   res.send('顯示一筆 Todo')
-})
+  next()
+}, printLog)
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   res.send('新增一筆  Todo')
-})
+  next()
+}, printLog)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
