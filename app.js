4d23d6ffcfcee854332cf5cee0af05960aa05e5f
Chris Chen <ok3201123@gmail.com>
2021-08-06 22:04:08 +0800

    feat: modify the timestamp middleware

--- app.js
+++ app.js
@@ -3,40 +3,29 @@ const moment = require('moment')
 const app = express()
 const port = 3000
 
-// 記錄request進入時間
+// 記錄request進入時間和回傳response時間並計算request-response cycle花費時間
 app.use((req, res, next) => {
   const reqTimestamp = Date.now()
-  res.locals.reqTimestamp = reqTimestamp
   next()
-})
-
-// 記錄回傳response時間並計算request-response cycle花費時間
-const printLog = (req, res) => {
-  const { reqTimestamp } = res.locals
   const resTimestamp = Date.now()
   console.log(`${moment(reqTimestamp).format('YYYY-MM-DD HH:mm:ss')} | ${req.method} from ${req.originalUrl} | total time: ${resTimestamp - reqTimestamp}ms`)
-  res.end()
-}
+})
 
 app.get('/', (req, res, next) => {
   res.send('列出全部 Todo')
-  next()
-}, printLog)
+})
 
 app.get('/new', (req, res, next) => {
   res.send('新增 Todo 頁面')
-  next()
-}, printLog)
+})
 
 app.get('/:id', (req, res, next) => {
   res.send('顯示一筆 Todo')
-  next()
-}, printLog)
+})
 
 app.post('/', (req, res, next) => {
   res.send('新增一筆  Todo')
-  next()
-}, printLog)
+})
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`)
